f58a16ea96d11022ccde602bccde3a9cf59bf191
Tons <tonyboutros123987@gmail.com>
2026-04-12 21:54:59 +0300

    Rename `AniWatchIE` class to `KaidoIE`

--- yt_dlp_plugins/extractor/kaido.py
+++ yt_dlp_plugins/extractor/kaido.py
@@ -12,7 +12,7 @@ from yt_dlp.utils import ExtractorError, clean_html, get_element_by_class
 from megacloud import Megacloud
 
 
-class AniWatchIE(InfoExtractor):
+class KaidoIE(InfoExtractor):
     _VALID_URL = r'https?://kaido\.to/(?:watch/)?(?P<slug>[^/?]+)(?:-\d+)?-(?P<playlist_id>\d+)(?:\?ep=(?P<episode_id>\d+))?$'
 
     _TESTS = [
