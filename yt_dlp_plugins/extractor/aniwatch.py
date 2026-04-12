eea438f19ff2ec38583bc9f016370dfa590e036e
Tons <tonyboutros123987@gmail.com>
2026-04-12 21:54:28 +0300

    Rename `AniWatchTVIE` class to `AniwatchIE`

--- yt_dlp_plugins/extractor/aniwatch.py
+++ yt_dlp_plugins/extractor/aniwatch.py
@@ -12,7 +12,7 @@ from yt_dlp.utils import ExtractorError, clean_html, get_element_by_class
 from megacloud import Megacloud
 
 
-class AniWatchTVIE(InfoExtractor):
+class AniwatchIE(InfoExtractor):
     _VALID_URL = r'https?://aniwatchtv\.to/(?:watch/)?(?P<slug>[^/?]+)(?:-\d+)?-(?P<playlist_id>\d+)(?:\?ep=(?P<episode_id>\d+))?$'
 
     _TESTS = [
